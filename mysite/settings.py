ce13018a8b84946d8bd950333f757d8ce32da439
steve <steven.olianga@gmail.com>
2019-12-07 18:11:19 +0300

    commit all

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        #'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'djangogirls',
+        'USER': 'bikopython',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '',
 
     }
